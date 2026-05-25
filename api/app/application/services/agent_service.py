e509ad684ba487cd32f43147c2a1d64a7ddb8be7
emcikem <2287697454@qq.com>
2026-05-25 23:49:16 +0800

    fix(.service): session_router

--- api/app/application/services/agent_service.py
+++ api/app/application/services/agent_service.py
@@ -140,21 +140,21 @@ class AgentService:
             async with self._uow:
                 session = await self._uow.session.get_by_id(session_id)
             if not session:
-                logger.error(f"尝试与不存在的任务会话[{session_id}]会话")
-                raise RuntimeError("任务会话不存在，请核实后重试")
+                logger.error(f"尝试与不存在的任务会话[{session_id}]对话")
+                raise RuntimeError("任务会话不存在, 请核实后重试")
 
-            # 2.获取对应的会话
+            # 2.获取对应会话任务
             task = await self._get_task(session)
 
             # 3.判断是否传递了message
             if message:
-                # 4.判断会话的状态是什么，如果不是运行中则表示已完成或者空闲中
-                if session.status != SessionStatus.RUNNING:
+                # 4.判断会话的状态是什么,如果不是运行中则表示已完成或者空闲中
+                if session.status != SessionStatus.RUNNING or task is None:
                     # 5.不在运行中需要创建一个新的task并启动
                     task = await self._create_task(session)
                     if not task:
-                        logger.error(f"会话[{session}]创建任务失败")
-                        raise RuntimeError(f"会话[{session}]创建任务失败")
+                        logger.error(f"会话[{session_id}]创建任务失败")
+                        raise RuntimeError(f"会话[{session_id}]创建任务失败")
 
                 # 6.传递了消息则更新会话中的最后一条消息
                 async with self._uow:
@@ -164,11 +164,16 @@ class AgentService:
                         timestamp=timestamp or datetime.now(),
                     )
 
+                # bugfix:从文件数据库中查询数据并更新attachments实际内容, 并返回人类消息事件
+                async with self._uow:
+                    db_attachments = [await self._uow.file.get_by_id(id) for id in attachments]
+
                 # 7.创建一个人类消息事件
                 message_event = MessageEvent(
                     role="user",
                     message=message,
-                    attachments=[File(id=attachment) for attachment in attachments] if attachments else [],
+                    attachments=[attachment for attachment in db_attachments if attachment is not None],
+                    # attachments=[File(id=attachment) for attachment in attachments] if attachments else [],
                 )
 
                 # 8.将事件添加到任务的输入流中，好让Agent获取到数据
@@ -180,11 +185,11 @@ class AgentService:
 
                 # 9.执行任务
                 await task.invoke()
-                logger.info(f"往会话[{session_id}]输入消息队列写入消息：{message[:50]}...")
+                logger.info(f"往会话[{session_id}]输入消息队列写入消息: {message[:50]}...")
 
             # 10.记录日志展示会话已启动
             logger.info(f"会话[{session_id}]已启动")
-            logger.info(f"会话[{session_id}]任务实例：{task}")
+            logger.info(f"会话[{session_id}]任务实例: {task}")
 
             # 11.从任务的输出流中读取数据
             while task and not task.done:
@@ -192,13 +197,13 @@ class AgentService:
                 event_id, event_str = await task.output_stream.get(start_id=latest_event_id, block_ms=0)
                 latest_event_id = event_id
                 if event_str is None:
-                    logger.debug(f"在会话[{session}]输出队列中未发现事件内容")
+                    logger.debug(f"在会话[{session_id}]输出队列中未发现事件内容")
                     continue
 
-                # 13.使用Pydantic提供的类型是配置器将event_str转换成指定类实例
+                # 13.使用Pydantic提供的类型适配器将event_str转换为指定类实例
                 event = TypeAdapter(Event).validate_json(event_str)
                 event.id = event_id
-                logger.debug(f"从会话[{session_id}]中获取事件：{type(event).__name__}")
+                logger.debug(f"从会话[{session_id}]中获取事件: {type(event).__name__}")
 
                 # 14.将未读消息数重置为0
                 async with self._uow:
@@ -212,8 +217,8 @@ class AgentService:
             # 16.循环外面表示这次任务AI端的已结束
             logger.info(f"会话[{session_id}]本轮运行结束")
         except Exception as e:
-            # 17.记录日志并返回错误日志
-            logger.error(f"任务会话[{session_id}]对话错误：{str(e)}")
+            # 17.记录日志并返回错误事件
+            logger.error(f"任务会话[{session_id}]对话出错: {str(e)}")
             event = ErrorEvent(error=str(e))
             try:
                 async with self._uow:
